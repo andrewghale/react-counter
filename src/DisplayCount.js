ee59060016e753ea20e4343e0be0494d7d677f98
Andrew Hale <andyhalebmx@hotmail.com>
2020-01-14 13:52:25 +0000

    making components

---
+++ src/DisplayCount.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const DisplayCount = (props) => {
+    return (
+        <div>
+            <p>
+                Count: {props.count}
+            </p>
+        </div>
+    )
+}
+
+export default DisplayCount
